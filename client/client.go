b7a32b648453e60c3414ad6b1202781875cfe689
Nathaniel Parrish <parrish.nate@gmail.com>
2015-08-28 11:22:33 -0700

    added function to pull files from local dir

--- client/client.go
+++ client/client.go
@@ -8,15 +8,15 @@ import (
   "io/ioutil"
 )
 
-func check(e error) {
-    if e != nil {
-      panic(e)
+func check(err error) {
+    if err != nil {
+      panic(err)
     }
 }
 
-func handleConnection(c net.Conn) {
+func handleConnection(connection net.Conn) {
 
-  log.Printf("Server %v connected.", c.RemoteAddr())
+  log.Printf("Server %v connected.", connection.RemoteAddr())
 
   // stuff to do... like read data from client, process it, write back to client
   // see what you can do with (c net.Conn) at
@@ -27,6 +27,7 @@ func handleConnection(c net.Conn) {
   dat, err := ioutil.ReadFile("../media/Wzlogo.mp3")
   check(err)
   max := 0
+  //buffers every 4096 bytes
   for i := 0; i < len(dat); i += 4096 {
     if i + 4096 < len(dat) {
       max = i + 4096
@@ -34,25 +35,33 @@ func handleConnection(c net.Conn) {
       max = len(dat) - 1
     }
     //sending to server
-    n, err := c.Write([]byte(dat[i:max]))
+    length, err := connection.Write([]byte(dat[i:max]))
     if err != nil {
-      c.Close()
+      connection.Close()
     }
     //server reads the file
-    n, err = c.Read(buffer)
-    if err != nil || n == 0 {
-      c.Close()
+    length, err = connection.Read(buffer)
+    if err != nil || length == 0 {
+      connection.Close()
     }
     fmt.Println(buffer);
   }
   
-  log.Printf("Connection from %v closed.", c.RemoteAddr())
+  log.Printf("Connection from %v closed.", connection.RemoteAddr())
+}
+
+func getFiles(dir string) {
+  files, err := ioutil.ReadDir(dir)
+  check(err)
+  for _, file := range files { 
+    fmt.Println(file.Name())
+  }
 }
 
 func main() {
   // resp, err := http.Get("http://media-stream-1049.appspot.com/test")
 
-  hostName := "localhost" // change this
+  hostName := "" // change this
   portNum := "3000"
   conn, err := net.Dial("tcp", hostName+":"+portNum)
   
@@ -68,9 +77,10 @@ func main() {
   // fmt.Println(s)
 
   handleConnection(conn)
+  // getFiles("../")
 
-  // fmt.Printf("Connection established between %s and localhost.\n", hostName)
-  // fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
-  // fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
+  fmt.Printf("Connection established between %s and localhost.\n", hostName)
+  fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
+  fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
 
 }
