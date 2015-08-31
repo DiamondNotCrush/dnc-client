be02e3bd6c11f2a712599476b3eb7e007fc7f15b
Nathaniel Parrish <parrish.nate@gmail.com>
2015-08-31 14:31:54 -0700

    refactored code to allow a specific directory to be shared recursively and files to only be served up if they are part of the list of shared files

--- client-server/client-server.go
+++ client-server/client-server.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+  "fmt"
   "log"
+  "strings"
   "net/http"
+  "encoding/json"
   "github.com/dnc/dnc-client/helper"
 )
 
 func main() {
-  http.HandleFunc("/", helper.ListOfFiles)
-  http.HandleFunc("/mpd/", helper.FindMpd)
+  dir := "../mpd/"
+  sharedFiles := helper.ListFiles(dir)
+
+  // respond with shared files
+  http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+    sharedFiles = helper.ListFiles(dir)
+    js, err := json.Marshal(sharedFiles)
+    helper.Check(err)
+    res.Header().Set("Content-Type", "application/json")
+    res.Write(js)
+  })
+
+  // serves up files if they are in the sharedFiles
+  http.HandleFunc("/mpd/", func(res http.ResponseWriter, req *http.Request) {
+    path := dir+strings.Join(strings.Split(req.URL.Path, "/")[2:], "")
+    if sharedFiles[path] {
+      fmt.Printf("Serving File: ")
+      fmt.Println(path)
+      http.ServeFile(res, req, path)
+    } else {
+      fmt.Printf("Blocking File: ")
+      fmt.Println(path)
+    }
+  })
 
   log.Println("Listening on port 3000")
   http.ListenAndServe(":3000", nil)
--- helper/helper.go
+++ helper/helper.go
@@ -1,47 +1,29 @@
 package helper
 
 import (
-  "os"
-  "fmt"
-  "encoding/json"
-  "net/http"
   "io/ioutil"
 )
 
-func check(err error) {
+func Check(err error) {
     if err != nil {
       panic(err)
     }
 }
 
-func ListOfFiles(res http.ResponseWriter, req *http.Request) {
-  files, err := ioutil.ReadDir("../media")
-  check(err)
-  var fileArray []string
+func listRecursion(dir string, fileObj map[string]bool) {
+  files, err := ioutil.ReadDir(dir)
+  Check(err)
   for _, file := range files {
-    fileArray = append(fileArray, file.Name())
+    if file.IsDir() {
+      listRecursion(dir+file.Name(), fileObj)
+    } else {
+      fileObj[dir+file.Name()] = true
+    }
   }
-  js, err := json.Marshal(fileArray)
-  res.Header().Set("Content-Type", "application/json")
-  res.Write(js)
 }
 
-func Exists(name string) bool {
-  _, err := os.Stat(name)
-  if err != nil {
-    return false
-  }
-  return true
-}
-
-func FindMpd(res http.ResponseWriter, req *http.Request) {
-  path := ".." + req.URL.Path
-  if !Exists(path) {
-    fmt.Printf("Convert File: ")
-    fmt.Println(path)
-  } else {
-    fmt.Printf("Play File: ")
-    fmt.Println(path)
-    http.ServeFile(res, req, path)
-  }
+func ListFiles(dir string) (map[string]bool) {
+  var fileObj = make(map[string]bool)
+  listRecursion(dir, fileObj)
+  return fileObj
 }
