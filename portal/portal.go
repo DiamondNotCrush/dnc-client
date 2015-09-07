8c1d6279eb36e72bcf6bf6381f46087d6277d7ac
Nathaniel Parrish <parrish.nate@gmail.com>
2015-09-07 16:03:35 -0700

    added bootstrap styling to admin portal

--- portal/portal.go
+++ portal/portal.go
@@ -6,15 +6,23 @@ import (
 )
 
 type Page struct {
+	Title       string
 	Dir         string
 	Port        string
 	LoginStatus string
 	Verify      string
 }
 
-var tmain, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/index.html")
-var tsignup, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/signup.html")
-var tlogin, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/login.html")
+var templates = template.Must(template.ParseFiles(
+	"../src/github.com/dnc/dnc-client/portal/templates/header.html",
+	"../src/github.com/dnc/dnc-client/portal/templates/footer.html",
+	"../src/github.com/dnc/dnc-client/portal/templates/main.html",
+	"../src/github.com/dnc/dnc-client/portal/templates/signup.html",
+	"../src/github.com/dnc/dnc-client/portal/templates/login.html"))
+
+// var tmain, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/main.html")
+// var tsignup, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/signup.html")
+// var tlogin, _ = template.ParseFiles("../src/github.com/dnc/dnc-client/portal/templates/login.html")
 
 //check user status
 func MainPage(res http.ResponseWriter, req *http.Request, dir string, port string, result bool, userid int) {
@@ -26,16 +34,18 @@ func MainPage(res http.ResponseWriter, req *http.Request, dir string, port strin
 	if userid > -1 {
 		status = `<span style="color:green">Logged in!</span>`
 	}
-	page := Page{dir, port, status, verify}
-	tmain.Execute(res, page)
+	// page := Page{dir, port, status, verify}
+	templates.ExecuteTemplate(res, "main", &Page{"Home", dir, port, status, verify})
 }
 
 //redirect to signup html
 func Signup(res http.ResponseWriter, req *http.Request) {
-	tsignup.Execute(res, nil)
+	data := struct{ Title string }{"Signup"}
+	templates.ExecuteTemplate(res, "signup", data)
 }
 
 //redirect to login html
 func Login(res http.ResponseWriter, req *http.Request) {
-	tlogin.Execute(res, nil)
+	data := struct{ Title string }{"Login"}
+	templates.ExecuteTemplate(res, "login", data)
 }
